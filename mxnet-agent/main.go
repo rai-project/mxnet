bef8e5708d39b7609390f4940a6ccb71e35e8703
Abdul Dakkak <abduld@wolfram.com>
2017-09-27 13:57:13 -0500

    add close method for tracers

--- mxnet-agent/main.go
+++ mxnet-agent/main.go
@@ -7,6 +7,7 @@ import (
 	cmd "github.com/rai-project/dlframework/framework/cmd/server"
 	"github.com/rai-project/mxnet"
 	_ "github.com/rai-project/mxnet/predict"
+	"github.com/rai-project/tracer"
 )
 
 func main() {
@@ -16,6 +17,7 @@ func main() {
 		os.Exit(-1)
 	}
 
+	defer tracer.Close()
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
