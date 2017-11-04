2aadca544f447f4ed103cf7e22cc9c175dbaa199
Abdul Dakkak <abduld@wolfram.com>
2017-11-04 15:13:48 -0500

    work on dataset inference

--- predict/predict.go
+++ predict/predict.go
@@ -100,6 +100,7 @@ func (p *ImagePredictor) GetPreprocessOptions(ctx context.Context) (common.Prepr
 	}
 
 	return common.PreprocessOptions{
+		Context:   ctx,
 		MeanImage: mean,
 		Scale:     scale,
 		Size:      []int{int(imageDims[1]), int(imageDims[2])},
